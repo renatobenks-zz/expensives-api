7645d363e523bc3c347e3b94d61a066cac5ea9ab
Renato Benkendorf <renato-benkendorf@hotmail.com>
2016-10-14 20:01:33 -0300

    Change new structure

--- calc.py
+++ calc.py
@@ -1,18 +1,21 @@
 #!/bin/env python3
-#coding: utf-8
+# coding: utf-8
 
+import json
+import sys
+import pprint
 from operator import itemgetter
 
+
 def transforma_csv_em_lista(dividas):
-    ''' Recebe os dados que vieram de um arquivo csv e devolve-os numa lista de tuplas'''
     lista = []
     for divida in dividas:
         nome, valor = divida.split(';')
         lista.append((nome, float(valor)))
     return sorted(lista)
 
+
 def agrupa_dividas_por_nome(dividas):
-    ''' Recebe os dados das dividas dos clientes e os agrupa num dicionário por nome do cliente'''
     dic = {}
     for divida in dividas:
         group_dividas = []
@@ -22,25 +25,39 @@ def agrupa_dividas_por_nome(dividas):
         dic[divida[0]] = group_dividas
     return dic
 
+
 def soma_dividas_por_nome(dividas):
-    ''' Recebe o dicionário com as dívidas dos clientes e devolve um dicionário com a soma da dívida de cada cliente'''
     for divida in dividas:
         dividas[divida] = round(sum(dividas[divida]), 2)
     return dividas
 
+
 def top_dividas(dividas, n=None):
-    '''Recebe o dicionário com os totais e o ordena, devolvendo os n maiores devedores.
-    Se n não for passado, devolve todos.'''
     return sorted(dividas.items(), key=itemgetter(1), reverse=True)[:n]
 
-if __name__ == '__main__':
+
+def calc():
     csv = open('selma_contas.csv').readlines()
     lista_de_dividas = transforma_csv_em_lista(csv)
     dic_dividas = agrupa_dividas_por_nome(lista_de_dividas)
-    dic_dividas_cliente = soma_dividas_por_nome(dic_dividas)
-    top_n = 4
-    lista_top_dividas = top_dividas(dic_dividas_cliente, top_n)
+    soma = soma_dividas_por_nome(dic_dividas)
+    if len(sys.argv) == 2 and sys.argv[1] == 'json':
+        generateJSON(soma)
+        print('JSON parsed and generated')
+    else:
+        print('Object returned with sum of expenses:')
+        pprint.pprint(soma)
 
-    print('Lista dos %d maiores devedores: ' %(top_n))
-    for cliente, valor in lista_top_dividas:
-        print('%-30s: R$%5.2f' %(cliente, valor))
+
+def generateJSON(dividas):
+    dividas = {
+        'name': 'Lanchonete da Selma',
+        'expenses': dividas
+    }
+    with open('build/mongo.json', 'w') as fb:
+        json.dump(dividas, fb)
+    fb.close()
+
+
+if __name__ == '__main__':
+    calc()
